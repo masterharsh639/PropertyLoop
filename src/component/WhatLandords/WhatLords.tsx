2b6892da86add6265356b3674f3f925bb4891fd5
harshmishraskugal <harshmishra@skugal.com>
2023-10-24 19:50:55 +0530

    first commit

--- src/component/WhatLandords/WhatLords.tsx
+++ src/component/WhatLandords/WhatLords.tsx
@@ -17,7 +17,7 @@ const WhatLord = () => {
       <Grid item xs={12}>
         <button className="getStarted-button">Get Started</button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} className="landlord-saving">
         <Grid item xs={4}>
           <p className="font-36">
             See how much you can save with PropertyLoop.
